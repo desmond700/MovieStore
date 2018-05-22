2c7713196a77f8905d8eef9b6aee5e6fecbe253f
desmond700 <dwdeveloper4@gmail.com>
2018-05-22 12:17:00 -0400

    Added some homepage content

--- index.php
+++ index.php
@@ -1,28 +1,44 @@
-<?php
-  require "model/database.php"
-  require "model/categorydb.php"
-  require "model/moviedb.php"
-  include "view/header.php"
+<?php include "view/header.php"; ?>
+    <div class="bg-dark py-4">
+      <div class="container" style="min-height: 650px;">
+        <div class="row">
+          <div class="d-flex flex-column col-12 col-md-6 px-0" style="min-height: 650px;">
+            <h2 class="text-white">Coming Soon</h2>
+            <div class="px-0 h-100">
+              <div class="container-fluid px-0 h-100">
+                <div class="row h-100">
+                  <div class="col-md-12 h-75 px-0 soonbgimg">
+                  </div>
+                  <div class="col-sm-6 col-md-6 h-25 px-0 soonsmleftimg">
+                  </div>
+                  <div class="col-sm-6 col-md-6 h-25 px-0 soonsmrightimg">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="d-flex flex-column col-12 col-md-6 px-0" style="min-height: 650px;">
+            <h2 class="text-white">In Theater</h2>
+            <div class="h-100 px-0">
+              <div class="container-fluid px-0 h-100">
+                <div class="row mx-0 h-100">
+                  <div class="col-sm-6 col-md-6 topleftimg h-25">
+                  </div>
+                  <div class="col-sm-6 col-md-6 toprightimg h-25">
+                  </div>
+                  <div class="d-flex col-md-12 btrightimg h-75">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
 
-  $action = filter_input(INPUT_POST, "action");
-  if($action == NULL){
-    $action = filter_input(INPUT_GET, "action");
-    if($action == NULL){
-      $action = "list_products";
-    }
-  }
+        </div>
 
-  if($action == ""){
 
-  }else if($action == ""){
+      </div>
+    </div>
 
-  }else if($action == ""){
-
-  }else if($action == ""){
-
-  }else if($action == ""){
-
-  }
-?>
+  </div>
 
 <?php include "view/footer.php" ?>
