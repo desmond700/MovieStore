adea97c4f16b55c82923dcf7d92def3a85fb6a4d
desmond700 <dwdeveloper4@gmail.com>
2018-05-22 22:39:37 -0400

    Added footer contents

---
+++ view/footer.php
@@ -0,0 +1,16 @@
+<footer class="py-4 bg-secondary">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 py-4">
+        <div>Quos amet optio nobis, ullam quod qui ratione, expedita quis, odit error, voluptate asperiores repellendus aliquam blanditiis voluptatibus autem accusantium. Necessitatibus accusamus quas, alias sed omnis perspiciatis, mollitia sit suscipit.</div>
+      </div>
+      <div class="col-md-4 py-4">
+        <div>Quos id fugit officia ipsa itaque quisquam eligendi nihil, ipsum, voluptatibus eum nesciunt perferendis dolorem, unde amet, rem quidem molestias cupiditate! Ab impedit quos inventore qui cum, ut ipsam incidunt.</div>
+      </div>
+      <div class="col-md-4 py-4">
+        <div>Libero aspernatur sit quidem sed modi ducimus adipisci, laborum, accusamus odit aliquam amet perferendis aperiam! Enim fugiat eaque quae tenetur sapiente asperiores. Nisi aspernatur, excepturi commodi sed dolores illo fugiat!</div>
+      </div>
+    </div>
+  </div>
+</footer>
+</body>
