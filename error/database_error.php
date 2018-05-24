952c22b68006a53b41f915a9fda738ed2bb56b94
desmond700 <dwdeveloper4@gmail.com>
2018-05-24 12:23:42 -0400

    Added error php

---
+++ error/database_error.php
@@ -0,0 +1,6 @@
+<?php include "view/header.php"; ?>
+  <h1>Database Error</h1>
+  <p>There was an error connecting to the database.</p>
+  <p>Error message: <?php echo $error_message; ?> </p>
+</div>
+<?php include "view/footer.php" ?>
