98419d5965eabe115180e35b397d123520c33928
desmond700 <dwdeveloper4@gmail.com>
2018-05-29 12:17:22 -0400

    Re-structured form layout

--- manage/movies/add.php
+++ manage/movies/add.php
@@ -3,39 +3,50 @@
   <main class="mb-4" style="margin-top: 5em">
     <div class="container">
       <h1>Add movie</h1>
+      <?php //foreach($actors as $actor) : ?>
+        <p><?php echo $_POST['actors'][2] ?></p>
+      <?php //endforeach ?>
       <hr>
       <div class="border px-4 py-4">
-        <form action="upload.php" method="post" enctype="multipart/form-data">
-
-        </form>
         <form class="col-md-8" action="?action=add_movie" method="post" enctype="multipart/form-data">
-          <div class="d-flex form-group">
+          <div class="d-flex form-group border-bottom pb-3">
             <label class="col-sm-4 col-md-3 pl-0">Title:&nbsp;</label>
             <input class="form-control form-control-sm" type="text" name="title" placeholder="Enter Movie's title">
           </div>
-          <div class="d-flex form-group">
-            <label class="col-sm-4 col-md-3 pl-0">Actors/Actresses:&nbsp;</label>
-            <input class="form-control form-control-sm" type="text" name="actors" placeholder="Enter Actor's/Actress's first and last names">
+          <div class="d-flex justify-content-between pl-0 border-bottom ">
+            <div class="form-group w-50 pr-2">
+              <label class="pl-0">Actors/Actresses:&nbsp;</label>
+              <input class="form-control form-control-sm mb-1" type="text" name="actors[]" placeholder="Enter Actor's/Actress's first and last names">
+              <input class="form-control form-control-sm mb-1" type="text" name="actors[]" placeholder="Enter Actor's/Actress's first and last names">
+              <input class="form-control form-control-sm mb-1" type="text" name="actors[]" placeholder="Enter Actor's/Actress's first and last names">
+              <input class="form-control form-control-sm mb-1" type="text" name="actors[]" placeholder="Enter Actor's/Actress's first and last names">
+              <input class="form-control form-control-sm mb-1" type="text" name="actors[]" placeholder="Enter Actor's/Actress's first and last names">
+            </div>
+            <div class="form-group w-50 pl-2 border-left">
+              <label class=" pl-0">Characters:&nbsp;</label>
+              <input class="form-control form-control-sm mb-1" type="text" name="characters[]" placeholder="Enter Characters's name">
+              <input class="form-control form-control-sm mb-1" type="text" name="characters[]" placeholder="Enter Characters's name">
+              <input class="form-control form-control-sm mb-1" type="text" name="characters[]" placeholder="Enter Characters's name">
+              <input class="form-control form-control-sm mb-1" type="text" name="characters[]" placeholder="Enter Characters's name">
+              <input class="form-control form-control-sm mb-1" type="text" name="characters[]" placeholder="Enter Characters's name">
+            </div>
           </div>
-          <div class="d-flex form-group">
-            <label class="col-sm-4 col-md-3 pl-0">Characters:&nbsp;</label>
-            <input class="form-control form-control-sm" type="text" name="characters" placeholder="Enter Characters's name">
-          </div>
-          <div class="d-flex form-group">
-            <label class="col-sm-4 col-md-3 pl-0">Description:&nbsp;</label>
-            <input class="form-control form-control-sm" type="text" name="overview" placeholder="Enter Movie's overview">
-          </div>
-          <div class="d-flex form-group">
+          <div class="d-flex form-group pt-3">
             <label class="col-sm-4 col-md-3 pl-0">Length:&nbsp;</label>
             <input class="form-control form-control-sm" type="text" name="runtime" placeholder="Enter Movie's Run time">
           </div>
           <div class="d-flex form-group">
             <label class="col-sm-4 col-md-3 pl-0">Genre/s:&nbsp;</label>
-            <input class="form-control form-control-sm" type="text" name="genres" placeholder="Enter Movie's genre/s">
+            <select name="sectionid" class="form-control">" >
+                <option value="">--Select a genre</option>
+                <?php $genresResults = get_genres();  foreach ($genresResults as $genre) : ?>
+                    <option value="<?php echo $genre["Genre"] ?>"><?php echo $genre["Genre"] ?></option>
+                <?php endforeach;?>
+            </select>
           </div>
           <div class="d-flex form-group">
             <label class="col-sm-4 col-md-3 pl-0">Release Date:&nbsp;</label>
-            <input class="form-control form-control-sm" type="text" name="releasedate" placeholder="Enter Movie's release date">
+            <input class="form-control form-control-sm" type="text" id="datepicker" name="releasedate" placeholder="Select Released Date">
           </div>
           <div class="d-flex form-group">
             <label class="col-sm-4 col-md-3 pl-0">Price:&nbsp;</label>
@@ -43,7 +54,14 @@
           </div>
           <div class="d-flex form-group">
             <label class="col-sm-4 col-md-3 pl-0">Rating:&nbsp;</label>
-            <input class="form-control form-control-sm" type="text" name="rating" placeholder="Enter Movie's rating">
+            <select name="rating" class="form-control">" >
+                <option value="">--Select a Rating</option>
+                <option value="G">G</option>
+                <option value="PG">PG</option>
+                <option value="PG-13">PG-13</option>
+                <option value="R">R</option>
+                <option value="NC-17">NC-17</option>
+            </select>
           </div>
           <div class="d-flex form-group">
             <label class="col-sm-4 col-md-3 pl-0">Overview:&nbsp;</label>
--- manage/movies/edit.php
+++ manage/movies/edit.php
@@ -10,19 +10,25 @@
       <div class="border px-4 py-4">
         <?php  ?>
         <form class="col-md-8" action="?action=add_movie" method="post" enctype="multipart/form-data">
-          <div class="d-flex form-group">
+          <div class="d-flex form-groupp border-bottom pb-3">
             <label class="col-sm-4 col-md-3 pl-0">Title:&nbsp;</label>
             <input class="form-control form-control-sm" type="text" name="title" value="<?php echo $movie["Title"] ?>">
           </div>
-          <div class="d-flex form-group">
-            <label class="col-sm-4 col-md-3 pl-0">Actors/Actresses:&nbsp;</label>
-            <input class="form-control form-control-sm" type="text" name="actors" value="<?php echo $actors ?>">
+          <div class="d-flex justify-content-between pl-0 pt-3 border-bottom ">
+            <div class="form-grou w-50 pr-2">
+              <label class="pl-0">Actors/Actresses:&nbsp;</label>
+              <?php foreach($actors as $actor) : ?>
+                <input class="form-control form-control-sm mb-1" type="text" value="<?php echo $actor["FirstName"]." ".$actor["LastName"] ?>" name="actors[]">
+              <?php endforeach ?>
+            </div>
+            <div class="form-group w-50 pl-2 border-left">
+              <label class=" pl-0">Characters:&nbsp;</label>
+              <?php foreach($characters as $character) : ?>
+                <input class="form-control form-control-sm mb-1" type="text" value="<?php echo $character["Name"] ?>" name="characters[]">
+              <?php endforeach ?>
+            </div>
           </div>
-          <div class="d-flex form-group">
-            <label class="col-sm-4 col-md-3 pl-0">Characters:&nbsp;</label>
-            <input class="form-control form-control-sm" type="text" name="characters" value="<?php echo $characters ?>">
-          </div>
-          <div class="d-flex form-group" id="directorInput">
+          <div class="d-flex form-group pt-3" id="directorInput">
             <label class="col-sm-4 col-md-3 pl-0">Director/s:&nbsp;</label>
             <input class="form-control form-control-sm" type="text" name="directors" value="<?php echo $movie["Title"] ?>">
           </div>
@@ -36,7 +42,7 @@
           </div>
           <div class="d-flex form-group">
             <label class="col-sm-4 col-md-3 pl-0">Release Date:&nbsp;</label>
-            <input class="form-control form-control-sm" type="text" name="releasedate" value="<?php echo $movie["Release_Date"] ?>">
+            <input class="form-control form-control-sm" type="text" name="releasedate"  id="datepicker" value="<?php echo $movie["Release_Date"] ?>">
           </div>
           <div class="d-flex form-group">
             <label class="col-sm-4 col-md-3 pl-0">Price:&nbsp;</label>
