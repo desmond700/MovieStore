4795e3db24221df97bc6033d3fb1b48b5bfec58e
desmond700 <dwdeveloper4@gmail.com>
2018-05-31 07:08:19 -0400

    Added script for modal variation

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 $(function(){
 
   $( "#datepicker" ).datepicker({ dateFormat: "yy-mm-dd" }).val();
-
+  $( "#tabs" ).tabs();
 
   $("button#mobilSrch").hide();
 
@@ -12,44 +12,61 @@ $(function(){
   //var mobileLogo = $("#mobiLogo").append(logo);
 
 
-  $( '.dropdown-menu button.dropdown-toggle' ).on( 'click', function ( e ) {
-        var $el = $( this );
-        var $parent = $( this ).offsetParent( ".dropdown-menu" );
-        if ( !$( this ).next().hasClass( 'show' ) ) {
-            $( this ).parents( '.dropdown-menu' ).first().find( '.show' ).removeClass( "show" );
-        }
-        var $subMenu = $( this ).next( ".dropdown-menu" );
-        $subMenu.toggleClass( 'show' );
+  // Get the modal
+  var modal = document.getElementById('id01');
 
-        $( this ).parent( "div" ).toggleClass( 'show' );
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
 
-        $( this ).parents( 'div.nav-item.dropdown.show' ).on( 'hidden.bs.dropdown', function ( e ) {
-            $( '.dropdown-menu .show' ).removeClass( "show" );
-        } );
-
-         if ( !$parent.parent().hasClass( 'navbar-nav' ) ) {
-            $el.next().css( { "top": $el[0].offsetBottom, "right": $parent.outerWidth() - $parent.width() } );
-        }
-
-        return false;
-  } );
-
-  var scntDiv = $('#directorInput');
-        var i = $('#directorInput p').size() + 1;
-
-        $('#addScnt').live('click', function() {
-                $('<p><label for="p_scnts"><input type="text" id="p_scnt" size="20" name="p_scnt_' + i +'" value="" placeholder="Input Value" /></label> <a href="#" id="remScnt">Remove</a></p>').appendTo(scntDiv);
-                i++;
-                return false;
-        });
-
-        $('#remScnt').live('click', function() {
-                if( i > 2 ) {
-                        $(this).parents('p').remove();
-                        i--;
-                }
-                return false;
-        });
+  $('#exampleModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var recipient = button.data('whatever') // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this);
+    //alert(recipient);
+    if(recipient === "login"){
+      var avatar = $("<i class='fa fa-user-circle text-success'></i>");
+      var imgdiv = $("<div class='imgcontainer my-0' style='font-size:110px'></div>").append(avatar);
+      var namelabel = $("<label class='d-flex align-items-center' for='uname'><b>Username: </b></label>")
+      var nameinput = $("<input class='form-control form-control-sm py-0' type='text' placeholder='Enter Username' style='line-height:6px' name='uname' required />")
+      var formgroup1 = $("<div class='d-flex form-group'></div>").append(namelabel,nameinput);
+      var passlabel = $("<label class='d-flex align-items-center' for='psw'><b>Password: </b></label>")
+      var passinput = $("<input class='form-control form-control-sm py-0' type='password' placeholder='Enter Password' name='uname' required />")
+      var formgroup2 = $("<div class='d-flex form-group'></div>").append(passlabel,passinput);
+      var loginbtn = $("<button type='submit'>Login</button>");
+      var rempassinput = $("<input type='checkbox' checked='checked' name='remember'> <span>Remember me</span>");
+      var rempassspan = $("<span class='psw pt-4'>Forgot <a href='#'>password?</a></span>");
+      var label = $("<label class='pt-4'></label>").append(rempassinput);
+      var flexdiv = $("<div class='d-flex justify-content-between'></div>").append(label,rempassspan);
+      var contdiv = $("<div class='container'></div>").append(formgroup1,formgroup2,loginbtn,flexdiv);
+      var form = $("<form class='modal-content animate my-auto' action='/action_page.php'></form>").append(imgdiv,contdiv);
+      modal.find('.modal-body').html(form)
+    }else if(recipient === "signup"){
+      var avatar = $("<i class='fa fa-user-circle text-success'></i>");
+      var imgdiv = $("<div class='imgcontainer my-0' style='font-size:110px'></div>").append(avatar);
+      var namelabel = $("<label class='d-flex align-items-center' for='uname' style='width:30%'><b>Username: </b></label>")
+      var nameinput = $("<input class='form-control form-control-sm py-0' type='text' placeholder='Enter Username' style='line-height:6px' name='uname' required />")
+      var formgroup1 = $("<div class='d-flex form-group'></div>").append(namelabel,nameinput);
+      var emaillabel = $("<label class='d-flex align-items-center' for='uname' style='width:30%'><b>Email: </b></label>")
+      var emailinput = $("<input class='form-control form-control-sm py-0' type='email' placeholder='Enter Email' style='line-height:6px' name='email' required />")
+      var formgroup2 = $("<div class='d-flex form-group'></div>").append(emaillabel,emailinput);
+      var passlabel = $("<label class='d-flex align-items-center' for='psw' style='width:30%'><b>Password: </b></label>")
+      var passinput = $("<input class='form-control form-control-sm py-0' type='password' placeholder='Enter Password' name='passw' required />")
+      var formgroup3 = $("<div class='d-flex form-group'></div>").append(passlabel,passinput);
+      var confpasslabel = $("<label class='d-flex align-items-center' for='psw' style='width:30%'><b>Confirm Password: </b></label>")
+      var confpassinput = $("<input class='form-control form-control-sm py-0' type='password' placeholder='Confirm Password' name='cpassw' required />")
+      var formgroup4 = $("<div class='d-flex form-group'></div>").append(confpasslabel,confpassinput);
+      var signupbtn = $("<button type='submit'>Signup</button>");
+      var contdiv = $("<div class='container'></div>").append(formgroup1,formgroup2,formgroup3,formgroup4,signupbtn);
+      var form = $("<form class='modal-content animate my-auto' action='/action_page.php'></form>").append(imgdiv,contdiv);
+      modal.find('.modal-body').html(form)
+    }
+  })
 })
 
 
