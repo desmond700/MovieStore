61e62e5cadfd3a8dca90222a599dfe02ad69009e
desmond700 <dwdeveloper4@gmail.com>
2018-05-31 21:45:08 -0400

    Added session for user purposes

--- view/header.php
+++ view/header.php
@@ -1,3 +1,12 @@
+<?php
+  session_start();
+
+  if(isset($_SESSION["is_loggedin"]) && $_SESSION["is_loggedin"]){
+    $user = $_SESSION["Username"];
+  }else{
+    $user = "guest";
+  }
+?>
 <!Doctype html>
 <html>
 <head>
@@ -22,29 +31,6 @@
   <script src="/MovieStore/js/main.js"></script>
 </head>
 <body>
-
-  <!-- The Modal -->
-  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-body">
-        <div id="tabs">
-          <ul class="bg-white border-0">
-            <li class="border-bottom"><a href="#tabs-1">User</a></li>
-            <li class="border-bottom"><a href="#tabs-2">Admin</a></li>
-          </ul>
-          <div id="tabs-1"></div>
-          <div id="tabs-2"></div>
-        </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-
   <div id="mySidenav" class="d-flex flex-column sidenav pt-0">
     <section class="d-flex">
       <a href="javascript:void(0)" class="closebtn d-flex my-auto py-0" onclick="closeNav()">x</a>
@@ -62,6 +48,10 @@
     <section class="mt-3">
       <h4 class="pl-3 ">Favourites-</h4>
 
+    </section>
+    <section class="mt-3">
+      <h4 class="pl-3 ">Administration-</h4>
+      <a href="/MovieStore/admin/">Admin</a>
     </section>
   </div>
   <div class="container-fluid px-0">
@@ -91,7 +81,7 @@
         </button>
         <div class="d-flex">
           <div class="d-flex border-left">
-            <a href="cart/" class="btn btn-light d-block d-flex my-auto h-100">
+            <a href="/MovieStore/cart/" class="btn btn-light d-block d-flex my-auto h-100">
               <i class="fa fa-shopping-cart my-auto text-dark" style="font-size:28px"></i>
             </a>
           </div>
@@ -100,60 +90,21 @@
             <button class="btn btn-light dropdown-toggle h-100 pb-0" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               <div class="d-flex flex-column">
                 <i class="fa fa-user-circle mx-auto" style="font-size:28px"></i>
-                <span class="my-auto">guest</span>
+                <span class="my-auto"><?php echo $user ?></span>
               </div>
             </button>
             <div class="dropdown-menu dropdown-menu-right px-2" aria-labelledby="dropdownMenuButton">
               <div class="d-flex">
                 <div class="dropdown my-auto h-100">
-                  <button class="btn btn-light h-100" type="button" id="dropdownMenuButton" data-toggle="modal" data-target="#exampleModal" data-whatever="login">
+                  <a href="/MovieStore/account/?action=login" class="btn btn-light h-100">
                     login
-                  </button>
-                  <div class="dropdown-menu dropdown-menu-right px-2" aria-labelledby="dropdownMenuButton" style="min-width:300px">
-                    <h4 class="text-center font-weight-bold">Log in</h4>
-                    <hr>
-                    <form class="form px-2">
-                      <div class="form-group">
-                        <label class=" font-weight-bold" for="exampleInputEmail1">Username:</label>
-                        <input type="email" class="form-control" aria-describedby="emailHelp" placeholder="Enter email">
-                      </div>
-                      <div class="form-group">
-                        <label class=" font-weight-bold" for="exampleInputPassword1">Password:</label>
-                        <input type="password" class="form-control" placeholder="Password">
-                      </div>
-                      <p><a href="#">forgot password?</a></p>
-                      <button type="submit" class="btn btn-primary float-right">Log in</button>
-                    </form>
-                  </div>
+                  </a>
                 </div>
 
                 <div class="dropdown my-auto h-100">
-                  <button class="btn btn-light h-100" type="button" id="dropdownMenuButton" data-toggle="modal" data-target="#exampleModal" data-whatever="signup">
+                  <a href="/MovieStore/account/?action=registration" class="btn btn-light h-100">
                     signup
-                  </button>
-                  <div class="dropdown-menu dropdown-menu-right px-2" aria-labelledby="dropdownMenuButton" style="min-width:300px">
-                    <h4 class="text-center font-weight-bold">Sign Up</h4>
-                    <hr>
-                    <form class="form px-2">
-                      <div class="form-group">
-                        <label class=" font-weight-bold" for="exampleInputEmail1">Username:</label>
-                        <input type="email" class="form-control" aria-describedby="emailHelp" placeholder="Enter username">
-                      </div>
-                      <div class="form-group">
-                        <label class=" font-weight-bold" for="exampleInputEmail1">Email:</label>
-                        <input type="email" class="form-control" aria-describedby="emailHelp" placeholder="Enter email">
-                      </div>
-                      <div class="form-group">
-                        <label class=" font-weight-bold" for="exampleInputPassword1">Password:</label>
-                        <input type="password" class="form-control" placeholder="Password">
-                      </div>
-                      <div class="form-group">
-                        <label class=" font-weight-bold" for="exampleInputPassword1">Confirm password:</label>
-                        <input type="password" class="form-control" placeholder="Password">
-                      </div>
-                      <button type="submit" class="btn btn-primary float-right">Sign up</button>
-                    </form>
-                  </div>
+                  </a>
                 </div>
               </div>
             </div>
