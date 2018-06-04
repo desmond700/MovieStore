d5fe4453bded0fb67f84e387cfd0b1e9285f9b2d
desmond700 <dwdeveloper4@gmail.com>
2018-06-04 12:09:01 -0400

    Replaced unnecessary codes

--- model/cart.php
+++ model/cart.php
@@ -6,10 +6,7 @@ if (!isset($_SESSION['cart']) ) {
 }
 
 function cart_add_item($product_id, $quantity) {
-    if(isset($_SESSION["user_id"]))
-      $_SESSION['cart'][$_SESSION["user_id"]][$product_id] = round($quantity, 0);
-    else
-      $_SESSION['cart']['guest'][$product_id] = round($quantity, 0);
+    $_SESSION['cart'][$product_id] = round($quantity, 0);
 
     // Set last category added to cart
     //$product = get_product($product_id);
@@ -21,42 +18,22 @@ function cart_add_item($product_id, $quantity) {
 function cart_get_items() {
     $items = array();
 
-    if(isset($_SESSION['cart'][$_SESSION['user_id']])){
-      foreach ($_SESSION['cart'][$_SESSION['user_id']] as $product_id => $quantity ) {
-          // Get product data from db
-          $product = get_a_movie_by_id($product_id);
-          $price = $product['Price'];
-          $quantity = intval($quantity);
+    foreach ($_SESSION['cart'] as $product_id => $quantity ) {
+        // Get product data from db
+        $product = get_a_movie_by_id($product_id);
+        $price = $product['Price'];
+        $quantity = intval($quantity);
 
-          // Calculate discount
-          $unitTotal = $price * $quantity;
+        // Calculate discount
+        $unitTotal = $price * $quantity;
 
-          // Store data in items array
-          $items[$product_id]['title'] = $product['Title'];
-          $items[$product_id]['poster'] = $product['Image_Name'];
-          $items[$product_id]['overview'] = $product['Overview'];
-          $items[$product_id]['price'] = $price;
-          $items[$product_id]['total'] = $unitTotal;
-          $items[$product_id]['quantity'] = $quantity;
-      }
-    }else if(isset($_SESSION['cart']["guest"])){
-      foreach (isset($_SESSION['cart']["guest"]) as $product_id => $quantity ) {
-          // Get product data from db
-          $product = get_a_movie_by_id($product_id);
-          $price = $product['Price'];
-          $quantity = intval($quantity);
-
-          // Calculate discount
-          $unitTotal = $price * $quantity;
-
-          // Store data in items array
-          $items[$product_id]['title'] = $product['Title'];
-          $items[$product_id]['poster'] = $product['Image_Name'];
-          $items[$product_id]['overview'] = $product['Overview'];
-          $items[$product_id]['price'] = $price;
-          $items[$product_id]['total'] = $unitTotal;
-          $items[$product_id]['quantity'] = $quantity;
-      }
+        // Store data in items array
+        $items[$product_id]['title'] = $product['Title'];
+        $items[$product_id]['poster'] = $product['Image_Name'];
+        $items[$product_id]['overview'] = $product['Overview'];
+        $items[$product_id]['price'] = $price;
+        $items[$product_id]['total'] = $unitTotal;
+        $items[$product_id]['quantity'] = $quantity;
     }
 
     return $items;
@@ -64,11 +41,7 @@ function cart_get_items() {
 
 
 function cart_product_count() {
-  if(isset($_SESSION['cart'][$_SESSION['user_id']])){
-    return count($_SESSION['cart'][$_SESSION['user_id']]);
-  }else if(isset($_SESSION['cart']['guest'])){
-    return count($_SESSION['cart']['guest']);
-  }
+  return count($_SESSION['cart']);
 }
 
 
